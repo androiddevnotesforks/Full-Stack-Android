c5329b8b121e5f3c69e5c58d23826b401daef8e8
TaeseongYun <yts0646@gmail.com>
2021-01-21 00:56:12 +0900

    feat: #2 라벨링 투명액티비티 처리, 다이얼로그 show 처리 완료

--- app/src/main/java/com/nexters/fullstack/ui/activity/LabelingActivity.kt
+++ app/src/main/java/com/nexters/fullstack/ui/activity/LabelingActivity.kt
@@ -4,13 +4,41 @@ import android.os.Bundle
 import com.nexters.fullstack.base.BaseActivity
 import com.nexters.fullstack.databinding.ActivityLabellingBinding
 import com.nexters.fullstack.R
+import com.nexters.fullstack.widget.CustomDialog
 
 class LabelingActivity : BaseActivity<ActivityLabellingBinding>() {
     override val layoutRes: Int = R.layout.activity_labelling
 
+    private val dialog by lazy {
+        CustomDialog.Builder(context = this, themeLayoutId = R.style.Transparent).build()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        initToolbar()
+        initDialog()
 //        bind {  }
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_chevron_left_24)
+    }
+
+    private fun initDialog() {
+        dialog.setOnCancelListener {
+            dialog.dismiss()
+            onSupportNavigateUp()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        dialog.show()
+//        onBackPressed()
+        return true
     }
 }
---
+++ app/src/main/java/com/nexters/fullstack/widget/CustomDialog.kt
@@ -0,0 +1,67 @@
+package com.nexters.fullstack.widget
+
+import android.app.Dialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.UiThread
+import androidx.databinding.DataBindingUtil
+import com.nexters.fullstack.R
+import com.nexters.fullstack.databinding.DialogInterfaceLayoutBinding
+
+class CustomDialog(private val builder: Builder) : Dialog(builder.context, builder.themeLayoutId) {
+
+    init {
+        builder.dialog = initCustomDialog(builder)
+    }
+
+
+    @UiThread
+    private fun initCustomDialog(builder: Builder): Dialog {
+        val dialog = Dialog(builder.context, builder.themeLayoutId)
+
+        dialog.setContentView(R.layout.dialog_interface_layout)
+
+        val tvContinue = findViewById<TextView>(R.id.tv_continue)
+        val tvExit = findViewById<TextView>(R.id.tv_exit)
+
+        tvContinue?.setOnClickListener(builder.setOnContinueListener)
+        tvExit?.setOnClickListener(builder.setOnCancelListener)
+        return dialog
+    }
+
+    @UiThread
+    override fun show() {
+        builder.dialog?.show()
+    }
+
+    @UiThread
+    override fun dismiss() {
+        builder.dialog?.dismiss()
+    }
+
+
+    class Builder(internal val context: Context, internal val themeLayoutId: Int) {
+        internal var dialog: Dialog? = null
+        internal var setOnContinueListener: SetOnContinueListener? = null
+        internal var setOnCancelListener: SetOnCancelListener? = null
+
+
+        @UiThread
+        fun build() = CustomDialog(this)
+
+        fun setOnContinueListener(listener: SetOnContinueListener): Builder {
+            this.setOnContinueListener = listener
+            return this
+        }
+
+        fun setOnCancelListener(listener: SetOnCancelListener): Builder {
+            this.setOnCancelListener = listener
+            return this
+        }
+    }
+}
+
+typealias SetOnContinueListener = (View) -> Unit
+typealias SetOnCancelListener = (View) -> Unit
