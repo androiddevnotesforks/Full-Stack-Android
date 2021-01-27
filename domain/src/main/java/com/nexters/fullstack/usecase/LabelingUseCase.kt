8c44099129bc12414841ade915a816ecb169d07a
gowoon-choi <gowoonvv@gmail.com>
2021-01-27 20:30:25 +0900

    feat: viewmodel usecase 생성

---
+++ domain/src/main/java/com/nexters/fullstack/usecase/LabelingUseCase.kt
@@ -0,0 +1,4 @@
+package com.nexters.fullstack.usecase
+
+class LabelingUseCase {
+}
--- presentation/src/main/java/com/nexters/fullstack/viewmodel/LabelOutAppViewModel.kt
+++ presentation/src/main/java/com/nexters/fullstack/viewmodel/LabelOutAppViewModel.kt
@@ -2,8 +2,44 @@ package com.nexters.fullstack.viewmodel
 
 import android.net.Uri
 import android.util.Log
+import androidx.lifecycle.LiveData
 import com.nexters.fullstack.BaseViewModel
+import com.nexters.fullstack.Input
+import com.nexters.fullstack.Output
+import com.nexters.fullstack.source.Label
 
 class LabelOutAppViewModel : BaseViewModel(){
 
 }
+
+interface LabelOutAppInput : Input {
+    fun clickCancel()
+    fun clickDone(labels : List<Label>)
+    fun selectLabel(label : Label)
+    fun unSelectLabel(label : Label)
+    fun enableSearch()
+    fun disableSearch()
+    fun searchKeyword(keyword : String)
+    // TODO search는 타이핑 할 때마다 ?
+    fun search()
+}
+
+interface LabelOutAppOutput : Output {
+    fun state() : LiveData<LabelOutAppState>
+    fun cancelLabeling()
+    // TODO 보기 클릭시에 어떤 화면으로 진입되는지 ?
+    fun successLabeling()
+    fun selectLabel() : LiveData<Pair<List<Label>, List<Label>>>
+    fun showSuccessToast() : LiveData<String>
+    fun showSearchList() : LiveData<Label>?
+    fun goAddLabelActivity() : LiveData<String>
+}
+
+data class LabelOutAppState (
+    val imageUri : Uri,
+    val selectedLabels : List<Label>,
+    val myLabels : List<Label>,
+    val enableSearch : Boolean,
+    val searchKeyword : String,
+    val searchResult : Label?
+)
