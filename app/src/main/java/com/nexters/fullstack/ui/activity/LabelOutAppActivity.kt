43b7c489021b5fa3fab7e16bfb681a0da7d073cb
gowoon-choi <gowoonvv@gmail.com>
2021-02-07 18:59:42 +0900

    fix : viewmodel 구조 변경

--- app/src/main/java/com/nexters/fullstack/ui/activity/LabelOutAppActivity.kt
+++ app/src/main/java/com/nexters/fullstack/ui/activity/LabelOutAppActivity.kt
@@ -1,6 +1,5 @@
 package com.nexters.fullstack.ui.activity
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.Rect
 import android.net.Uri
@@ -8,38 +7,33 @@ import android.os.Bundle
 import android.os.Parcelable
 import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.nexters.fullstack.Constants
 import com.nexters.fullstack.R
 import com.nexters.fullstack.base.BaseActivity
-import com.nexters.fullstack.base.BaseAdapter
 import com.nexters.fullstack.databinding.ActivityLabelOutappBinding
 import com.nexters.fullstack.ext.toPx
-import com.nexters.fullstack.source.Label
 import com.nexters.fullstack.ui.adapter.MyLabelAdapter
+import com.nexters.fullstack.ui.adapter.SelectedLabelAdapter
 import com.nexters.fullstack.viewmodel.LabelOutAppViewModel
 import com.xiaofeng.flowlayoutmanager.FlowLayoutManager
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LabelOutAppActivity : BaseActivity<ActivityLabelOutappBinding, LabelOutAppViewModel>() {
     override val layoutRes: Int = R.layout.activity_label_outapp
     override val viewModel: LabelOutAppViewModel by viewModel()
 
-    private val adapter : MyLabelAdapter = MyLabelAdapter()
+    private val myLabelAdapter : MyLabelAdapter = MyLabelAdapter()
+    private val selectedAdapter : SelectedLabelAdapter = SelectedLabelAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initData()
         initView()
-        initOnClickListener()
-        observe()
+//        initOnClickListener()
+//        observe()
     }
 
     private fun initData(){
@@ -47,40 +41,51 @@ class LabelOutAppActivity : BaseActivity<ActivityLabelOutappBinding, LabelOutApp
             intent?.action == Intent.ACTION_SEND
                     && intent.type?.startsWith(Constants.IMAGE_PREFIX) == true -> {
                 (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let { uri : Uri ->
-                    viewModel.input.setImageUri(uri)
+                    viewModel.input.loadImage(uri)
                 }
             }
         }
     }
 
     private fun initView(){
-        viewModel.imageUri.observe(this, {
-            Glide.with(this)
-                .load(it)
-                .into(binding.ivScreenshot)
-        })
-
-        val spaceDecoration = SpaceItemDecoration(RV_SPACING_DP)
-        adapter.addItems(viewModel.myLabels.value?: ArrayList())
-        binding.rvLabel.adapter = adapter
-        binding.rvLabel.addItemDecoration(spaceDecoration)
-        binding.rvLabel.layoutManager = FlowLayoutManager()
-        viewModel.myLabels.observe(this, {
-            adapter.calDiff(it)
-        })
-    }
-
-    private fun initOnClickListener(){
-       binding.ivCancel.setOnClickListener {
-            onBackPressed()
+        with(viewModel.output.state()){
+            imageUri.observe(this@LabelOutAppActivity, {
+                Glide.with(this@LabelOutAppActivity)
+                    .load(it)
+                    .into(binding.ivScreenshot)
+            })
         }
-        adapter.setItemClickListener { _, i, label ->
-
-        }
+//
+//        val spaceDecoration = SpaceItemDecoration(RV_SPACING_DP)
+//        myLabelAdapter.addItems(viewModel.myLabels.value?: ArrayList())
+//        binding.rvLabel.adapter = myLabelAdapter
+//        binding.rvLabel.addItemDecoration(spaceDecoration)
+//        binding.rvLabel.layoutManager = FlowLayoutManager()
+//
+//        selectedAdapter.addItems(viewModel.selectedLabels.value?: ArrayList())
+//        binding.rvSelectedLabel.adapter = selectedAdapter
+//        binding.rvSelectedLabel.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
     }
-
-    private fun observe(){
-    }
+//
+//    private fun initOnClickListener(){
+//       binding.ivCancel.setOnClickListener {
+//            onBackPressed()
+//        }
+//        myLabelAdapter.setItemClickListener { _, i, _ ->
+//            viewModel.output.selectLabel(i)
+//        }
+//    }
+//
+//    private fun observe(){
+//        with(viewModel){
+//            myLabels.observe(this@LabelOutAppActivity, {
+//                myLabelAdapter.calDiff(it)
+//            })
+//            selectedLabels.observe(this@LabelOutAppActivity, {
+//
+//            })
+//        }
+//    }
 
     inner class SpaceItemDecoration(private val space_dp: Int) : RecyclerView.ItemDecoration() {
         override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
@@ -88,6 +93,7 @@ class LabelOutAppActivity : BaseActivity<ActivityLabelOutappBinding, LabelOutApp
             outRect.right = space_dp.toPx
         }
     }
+
     companion object{
         const val RV_SPACING_DP = 10
     }
--- presentation/src/main/java/com/nexters/fullstack/viewmodel/LabelOutAppViewModel.kt
+++ presentation/src/main/java/com/nexters/fullstack/viewmodel/LabelOutAppViewModel.kt
@@ -1,6 +1,7 @@
 package com.nexters.fullstack.viewmodel
 
 import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -8,68 +9,65 @@ import com.nexters.fullstack.BaseViewModel
 import com.nexters.fullstack.Input
 import com.nexters.fullstack.Output
 import com.nexters.fullstack.source.Label
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class LabelOutAppViewModel : BaseViewModel(){
+class LabelOutAppViewModel : BaseViewModel() {
+    private lateinit var state : State
+//    val myLabels = MutableLiveData<ArrayList<Label>>()
+//    private val _selectedLabels: MutableLiveData<ArrayList<Label>> = MutableLiveData()
+//    val selectedLabels: LiveData<ArrayList<Label>> get() = _selectedLabels
 
-    private val _imageUri : MutableLiveData<Uri> = MutableLiveData()
-    val imageUri : LiveData<Uri> get() = _imageUri
-
-    private val _myLabels : MutableLiveData<ArrayList<Label>> = MutableLiveData()
-    val myLabels : LiveData<ArrayList<Label>> get() = _myLabels
-
-
-    val input = object : LabelOutAppInput{
-        override fun setImageUri(uri: Uri) {
-            _imageUri.value = uri
+    val input = object : LabelOutAppInput {
+        override fun loadImage(uri: Uri) {
+            state.imageUri.value = uri
         }
 
-        override fun setMyLabel() {
-            val array = ArrayList<Label>()
-            viewModelScope.launch(Dispatchers.IO) {
-                array.add(Label(Label.RECOMMEND, "test1"))
-                array.add(Label(Label.DEFAULT,"test21111111"))
-                array.add(Label(Label.DEFAULT,"test3"))
-                array.add(Label(Label.DEFAULT,"te3"))
-                array.add(Label(Label.DEFAULT,"test3"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test3"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-                array.add(Label(Label.DEFAULT,"test11111113"))
-            }
-            _myLabels.value = array
-        }
+//        override fun setMyLabel() {
+//            val array = ArrayList<Label>()
+//            viewModelScope.launch {
+//                array.add(Label(Label.RECOMMEND, "test1"))
+//                array.add(Label(Label.DEFAULT, "test21111111"))
+//                array.add(Label(Label.DEFAULT, "test3"))
+//                array.add(Label(Label.DEFAULT, "te3"))
+//                array.add(Label(Label.DEFAULT, "test3"))
+//                array.add(Label(Label.DEFAULT, "test11111113"))
+//                array.add(Label(Label.DEFAULT, "test3"))
+//            }
+//            myLabels.value = array
+//        }
     }
 
     val output = object : LabelOutAppOutput {
+
+//        override fun selectLabel(selectedPosition: Int) {
+//            val selectedLabel: Label = myLabels.value!![selectedPosition]
+//            myLabels.value?.removeAt(selectedPosition)
+//            //selectedLabels.value?.add(selectedLabel)
+//            Log.e("selected item : ", selectedPosition.toString())
+//        }
+
+        override fun state(): State = state
     }
 
-    interface LabelOutAppInput : Input{
-        // set state
-        fun setImageUri(uri: Uri)
-        fun setMyLabel()
+    interface LabelOutAppInput : Input {
+        fun loadImage(uri: Uri)
     }
 
-    interface LabelOutAppOutput : Output{
+    interface LabelOutAppOutput : Output {
+        fun state() : State
     }
 
     init {
-        input.setMyLabel()
+        state = State()
     }
+
+    data class State(
+        // TODO image uri랑 my label, recommend label은 non null로 하고, init에서 state 정의할 때 받아와서 정의해주기 !
+        val imageUri : MutableLiveData<Uri> = MutableLiveData(),
+        val recommendedLabels: List<Label>? = null,
+        val myLabels: List<Label>? = null,
+        val selectedLabels : List<Label>? = null,
+        val enableSearch : Boolean = false,
+        val searchKeyword : String? = null
+    )
 }
