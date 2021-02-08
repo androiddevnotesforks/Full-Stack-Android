6d0e099a82968d337e0eebfa78da387c2df4e5c3
TaeseongYun <yts0646@gmail.com>
2021-02-08 10:53:00 +0900

    feat: depth 컬러 추가, LabelListViewHolder 추가 및 수정

---
+++ app/src/main/java/com/nexters/fullstack/ui/holder/LabelListViewHolder.kt
@@ -0,0 +1,8 @@
+package com.nexters.fullstack.ui.holder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.nexters.fullstack.databinding.ItemListLabelBinding
+
+class LabelListViewHolder(private val binding: ItemListLabelBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+}
