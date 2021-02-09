0e7dbf255cb0bda17ea755035e492be9b531e08f
TaeseongYun <yts0646@gmail.com>
2021-02-10 00:04:39 +0900

    fix: recycler decoration 코멘트 반영

--- app/src/main/java/com/nexters/fullstack/ui/fragment/LabelSelectFragment.kt
+++ app/src/main/java/com/nexters/fullstack/ui/fragment/LabelSelectFragment.kt
@@ -11,7 +11,7 @@ import com.nexters.fullstack.R
 import com.nexters.fullstack.source.LabelSource
 import com.nexters.fullstack.source.ViewState
 import com.nexters.fullstack.ui.adapter.LabelingSelectAdapter
-import com.nexters.fullstack.ui.decoration.LabelRecyclerDecoration
+import com.nexters.fullstack.ui.decoration.SpaceBetweenRecyclerDecoration
 import com.nexters.fullstack.viewmodel.LabelingViewModel
 import com.nexters.fullstack.widget.RequestExitDialog
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -31,7 +31,7 @@ class LabelSelectFragment : BaseFragment<FragmentLabelSelectBinding, LabelingVie
 //        labelAdapter.addItems(listOf(LabelSource(LabelSource.LIST, "#EC9147", "테스트")))
 
         binding.rvLabel.adapter = labelAdapter
-        binding.rvLabel.addItemDecoration(LabelRecyclerDecoration())
+        binding.rvLabel.addItemDecoration(SpaceBetweenRecyclerDecoration(vertical = 10))
 
         binding.tvAddLabel.setOnClickListener {
             viewModel.input.clickAppbar(ViewState.Add)
