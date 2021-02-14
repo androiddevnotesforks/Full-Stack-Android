a0e4301e9a9d554cc19d6777a0e3efe651cd11d7
TaeseongYun <yts0646@gmail.com>
2021-02-15 01:02:53 +0900

    fix: 라벨 클릭 state 적용 완료

--- app/src/main/java/com/nexters/fullstack/ui/holder/LabelListViewHolder.kt
+++ app/src/main/java/com/nexters/fullstack/ui/holder/LabelListViewHolder.kt
@@ -5,11 +5,29 @@ import androidx.recyclerview.widget.RecyclerView
 import com.nexters.fullstack.databinding.ItemListLabelBinding
 import com.nexters.fullstack.source.LabelSource
 
-class LabelListViewHolder(private val binding: ItemListLabelBinding) :
+class LabelListViewHolder(
+    private val onLabelClickListener: (position: Int) -> Unit,
+    private val binding: ItemListLabelBinding
+) :
     RecyclerView.ViewHolder(binding.root) {
+    init {
+        binding.labelView.setOnClickListener {
+            onLabelClickListener(adapterPosition)
+        }
+    }
 
-    fun bind(item: LabelSource) {
+    fun bind(selectedList: List<LabelSource>, item: LabelSource) {
         binding.label.text = item.name
-        binding.hintColor.setBackgroundColor(Color.parseColor(item.color))
+        binding.labelView.setBackgroundColor(Color.parseColor(item.color))
+        if (selectedList.contains(item)) {
+            binding.labelView.setBackgroundColor(Color.parseColor(item.color))
+        } else {
+            binding.hintColor.setBackgroundColor(Color.parseColor(item.color))
+            binding.labelView.setBackgroundColor(Color.parseColor(DEFAULT_COLOR))
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_COLOR = "#292C33"
     }
 }
