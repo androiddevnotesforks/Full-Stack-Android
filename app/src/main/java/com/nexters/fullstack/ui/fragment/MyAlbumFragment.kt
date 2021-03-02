efc412b85a1a4fa08b2c5bbc4480661390452df5
TaeseongYun <yts0646@gmail.com>
2021-03-02 20:53:27 +0900

    feat: 3_label bind create label
    라벨 생성 ui 연결

--- app/src/main/java/com/nexters/fullstack/ui/fragment/MyAlbumFragment.kt
+++ app/src/main/java/com/nexters/fullstack/ui/fragment/MyAlbumFragment.kt
@@ -1,25 +1,46 @@
 package com.nexters.fullstack.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.nexters.fullstack.base.BaseFragment
 import com.nexters.fullstack.databinding.FragmentMyalbumBinding
 import com.nexters.fullstack.R
-import com.nexters.fullstack.widget.CreateAlbumBottomSheet
+import com.nexters.fullstack.ui.activity.CreateLabelActivity
 import com.nexters.fullstack.viewmodel.MainViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subjects.BehaviorSubject
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.concurrent.TimeUnit
 
 class MyAlbumFragment : BaseFragment<FragmentMyalbumBinding, MainViewModel>() {
     override val layoutRes: Int = R.layout.fragment_myalbum
     override val viewModel: MainViewModel by viewModel()
+    private val addLabelButtonSubject = BehaviorSubject.create<Unit>()
+
+    init {
+        /**
+         * 액티비티 중복 호출 방지.
+         */
+        disposable.add(
+            addLabelButtonSubject
+                .throttleFirst(1000L, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        startActivity(Intent(requireContext(), CreateLabelActivity::class.java))
+                    }, {
+                        it.printStackTrace()
+                    }
+                )
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.addLabel.setOnClickListener {
-//            CreateAlbumBottomSheet(requireContext()).show()
-            //todo LabelCreate Activity
+            addLabelButtonSubject.onNext(Unit)
         }
     }
 
