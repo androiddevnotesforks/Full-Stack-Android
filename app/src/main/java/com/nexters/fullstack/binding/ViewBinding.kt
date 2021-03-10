2f5758227eeee4a7ae1892c6583e59138664f04f
TaeseongYun <yts0646@gmail.com>
2021-03-10 22:44:11 +0900

    feat : 로컬 db 리사이클러 적용 완료.
    로컬 db적용  to MyAlbumFragment RecyclerView

---
+++ app/src/main/java/com/nexters/fullstack/binding/ViewBinding.kt
@@ -0,0 +1,24 @@
+package com.nexters.fullstack.binding
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.nexters.fullstack.R
+
+object ViewBinding {
+    @JvmStatic
+    @BindingAdapter("app:localImage")
+    fun ImageView.setLocalImageThumbnail(url: String?) {
+        url?.let {
+            Glide.with(this)
+                .load(it)
+                .apply(RequestOptions().centerCrop())
+                .into(this)
+        } ?: run {
+            Glide.with(this)
+                .load(R.drawable.ic_ico_empty_screenshot)
+                .into(this)
+        }
+    }
+}
--- app/src/main/java/com/nexters/fullstack/ui/holder/LocalImageViewHolder.kt
+++ app/src/main/java/com/nexters/fullstack/ui/holder/LocalImageViewHolder.kt
@@ -1,8 +1,20 @@
 package com.nexters.fullstack.ui.holder
 
 import androidx.recyclerview.widget.RecyclerView
+import com.nexters.feature.util.ColorUtils
 import com.nexters.fullstack.databinding.RecyclerAlbumItemBinding
+import com.nexters.fullstack.source.local.DomainUserImage
 
 class LocalImageViewHolder(private val binding: RecyclerAlbumItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
+    fun onBind(item: DomainUserImage) {
+        binding.item = item
+//        binding.label.setBackgroundColor(
+//            ColorUtils(
+//                item.labels.first().color ?: "",
+//                binding.root.context
+//            ).getActive()
+//        )
+        binding.executePendingBindings()
+    }
 }
