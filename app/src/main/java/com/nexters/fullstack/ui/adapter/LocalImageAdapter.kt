75ffda59ad98fba3eb61c3156d7d14721f8fd0d8
TaeseongYun <yts0646@gmail.com>
2021-03-14 14:39:36 +0900

    fix : 라벨 item xml 수정
    더보기 이미지 추가, 이미지 사이즈 텍스트 뷰 수정

--- app/src/main/java/com/nexters/fullstack/ui/adapter/LocalImageAdapter.kt
+++ app/src/main/java/com/nexters/fullstack/ui/adapter/LocalImageAdapter.kt
@@ -4,19 +4,19 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.nexters.fullstack.base.BaseAdapter
-import com.nexters.fullstack.databinding.RecyclerAlbumItemBinding
+import com.nexters.fullstack.databinding.ItemAlbumWithLabelBinding
 import com.nexters.fullstack.source.local.DomainUserImage
 import com.nexters.fullstack.ui.holder.LocalImageViewHolder
 
 class LocalImageAdapter : BaseAdapter<DomainUserImage>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return LocalImageViewHolder(
-            RecyclerAlbumItemBinding.inflate(LayoutInflater.from(parent.context))
+            ItemAlbumWithLabelBinding.inflate(LayoutInflater.from(parent.context))
         )
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if(holder is LocalImageViewHolder) {
+        if (holder is LocalImageViewHolder) {
             holder.onBind(items[position])
         }
     }
--- app/src/main/java/com/nexters/fullstack/ui/holder/LocalImageViewHolder.kt
+++ app/src/main/java/com/nexters/fullstack/ui/holder/LocalImageViewHolder.kt
@@ -1,11 +1,10 @@
 package com.nexters.fullstack.ui.holder
 
 import androidx.recyclerview.widget.RecyclerView
-import com.nexters.feature.util.ColorUtils
-import com.nexters.fullstack.databinding.RecyclerAlbumItemBinding
+import com.nexters.fullstack.databinding.ItemAlbumWithLabelBinding
 import com.nexters.fullstack.source.local.DomainUserImage
 
-class LocalImageViewHolder(private val binding: RecyclerAlbumItemBinding) :
+class LocalImageViewHolder(private val binding: ItemAlbumWithLabelBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun onBind(item: DomainUserImage) {
         binding.item = item
