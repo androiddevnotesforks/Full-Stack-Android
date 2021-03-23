dcb847a8ded9c5c94a3fa41ebd57482139160e99
gowoon-choi <gowoonvv@gmail.com>
2021-03-23 16:11:34 +0900

    fix : indicator 수정

--- app/src/main/java/com/nexters/fullstack/ui/fragment/OnBoardingFragment.kt
+++ app/src/main/java/com/nexters/fullstack/ui/fragment/OnBoardingFragment.kt
@@ -28,12 +28,6 @@ class OnBoardingFragment : BaseFragment<FragmentOnboardingBinding, OnBoardingVie
         binding.lottie.setAnimation(LOTTIE_PREFIX + data.order + LOTTIE_SUFFIX)
         binding.lottie.playAnimation()
         binding.lottie.repeatCount = LottieDrawable.INFINITE
-
-        when(data.order){
-            1 -> binding.indicator1.isSelected = true
-            2 -> binding.indicator2.isSelected = true
-            3 -> binding.indicator3.isSelected = true
-        }
     }
 
     companion object {
