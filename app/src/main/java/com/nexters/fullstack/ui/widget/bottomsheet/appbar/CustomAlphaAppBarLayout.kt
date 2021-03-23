f0e52ec051f9fc1f31b578a89d9263842bbe115e
TaeseongYun <yts0646@gmail.com>
2021-03-23 22:32:48 +0900

    feat: 앱바 offset Listener

---
+++ app/src/main/java/com/nexters/fullstack/ui/widget/bottomsheet/appbar/CustomAlphaAppBarLayout.kt
@@ -0,0 +1,9 @@
+package com.nexters.fullstack.ui.widget.bottomsheet.appbar
+
+import com.google.android.material.appbar.AppBarLayout
+
+class CustomAlphaAppBarOffsetChangeListener : AppBarLayout.OnOffsetChangedListener {
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+
+    }
+}
