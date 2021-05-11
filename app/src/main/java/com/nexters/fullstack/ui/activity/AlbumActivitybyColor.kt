e1dc4e1ed0b6b750aa79ed6aba40cd65bd0f2893
TaeseongYun <yts0646@gmail.com>
2021-05-12 07:02:14 +0900

    feat : 설정 컬러값 적용

--- app/src/main/java/com/nexters/fullstack/ui/activity/AlbumActivitybyColor.kt
+++ app/src/main/java/com/nexters/fullstack/ui/activity/AlbumActivitybyColor.kt
@@ -6,34 +6,56 @@ import com.nexters.fullstack.Constants
 import com.nexters.fullstack.R
 import com.nexters.fullstack.base.BaseActivity
 import com.nexters.fullstack.databinding.ActivityAlbumActivitybyColorBinding
+import com.nexters.fullstack.mapper.LocalImageMapper
 import com.nexters.fullstack.source.LabelSource
+import com.nexters.fullstack.source.LocalImageData
 import com.nexters.fullstack.util.ColorUtil
 import com.nexters.fullstack.viewmodel.AlbumViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
+//todo 라벨 네임 컬러 정하기 리사이클러 뷰 데이터 init
 class AlbumActivitybyColor : BaseActivity<ActivityAlbumActivitybyColorBinding, AlbumViewModel>() {
 
     override val layoutRes: Int = R.layout.activity_album_activityby_color
     override val viewModel: AlbumViewModel by viewModel()
 
     lateinit var labelName: LabelSource
+    lateinit var labelColor: String
+    lateinit var images: List<LocalImageData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setInitData()
         bind {
             vm = viewModel
+            colorUtil = ColorUtil(viewModel.output.getLabelColor().value ?: labelColor)
         }
+        setObserve()
     }
 
     private fun setInitData() {
-        labelName = intent.getParcelableExtra(Constants.LABEL) ?: LabelSource(name = "N/A", color = "Yellow")
+        val labelSource = intent.getParcelableExtra(Constants.LABEL) ?: LabelSource(
+            name = "N/A",
+            color = "Yellow"
+        )
+        val intentImages = intent.getParcelableArrayListExtra<LocalImageData>(Constants.KEY_IMAGES)
+        labelName = labelSource
+        labelColor = labelSource.color
+        images = (intentImages?.toList() ?: emptyList())
+
+        val localImage = images.map(LocalImageMapper::fromDomain)
         with(viewModel.input) {
             setLabelName(labelName.name)
+            setImages(localImage)
         }
     }
 
     private fun setObserve() {
-
+        with(viewModel.output) {
+            finishActivity().observe(this@AlbumActivitybyColor) {
+                finish()
+            }
+        }
     }
+
 }
