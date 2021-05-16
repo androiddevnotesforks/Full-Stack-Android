a5071110551b7569a112966bdcee8a5955c7f962
gowoon-choi <gowoonvv@gmail.com>
2021-05-16 18:43:47 +0900

    fix: apply design qa

--- presentation/src/main/java/com/nexters/fullstack/viewmodel/HomeMainViewModel.kt
+++ presentation/src/main/java/com/nexters/fullstack/viewmodel/HomeMainViewModel.kt
@@ -19,6 +19,14 @@ class HomeMainViewModel : BaseViewModel() {
     init {
         // TODO load screenshots..!
         viewModelScope.launch {
+            recentScreenshotList.add(HomeScreenshot("https://homepages.cae.wisc.edu/~ece533/images/arctichare.png", null, false))
+            recentScreenshotList.add(HomeScreenshot("https://homepages.cae.wisc.edu/~ece533/images/airplane.png", null, false))
+            recentScreenshotList.add(HomeScreenshot("https://homepages.cae.wisc.edu/~ece533/images/airplane.png", null, false))
+            recentScreenshotList.add(HomeScreenshot("https://homepages.cae.wisc.edu/~ece533/images/airplane.png", null, false))
+            favoriteScreenshotList.add(HomeScreenshot("https://homepages.cae.wisc.edu/~ece533/images/airplane.png", null, true))
+            favoriteScreenshotList.add(HomeScreenshot("https://homepages.cae.wisc.edu/~ece533/images/airplane.png", null, true))
+            favoriteScreenshotList.add(HomeScreenshot("https://homepages.cae.wisc.edu/~ece533/images/airplane.png", null, true))
+            favoriteScreenshotList.add(HomeScreenshot("https://homepages.cae.wisc.edu/~ece533/images/airplane.png", null, true))
             screenshotGroupList.add(HomeList(HomeListType.RECENT, HomeListType.RECENT.title, recentScreenshotList))
             screenshotGroupList.add(HomeList(HomeListType.FAVORITE, HomeListType.FAVORITE.title,favoriteScreenshotList))
         }
